532a05d626ab8966d36f225b2ef3a850a05d5eab
mohammad-ki <114981226+mohammadkiyaie@users.noreply.github.com>
2026-07-29 11:00:57 +0330

    add contact section & smoth nav

--- logic.js
+++ logic.js
@@ -1,6 +1,20 @@
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const headerNav = document.querySelector('.header__nav');
+const hamburger = document.querySelector('.header__hamburger');
+const nav = document.querySelector('.header__nav');
+const navLinks = document.querySelectorAll('.header__nav-list a');
 
-hamburgerBtn.addEventListener('click', () => {
-    headerNav.classList.toggle('active');
+hamburger.addEventListener('click', (e) => {
+    e.stopPropagation();
+    nav.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !hamburger.contains(e.target)) {
+        nav.classList.remove('active');
+    }
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
 });
